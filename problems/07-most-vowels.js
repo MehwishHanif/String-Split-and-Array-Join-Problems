b45f2fbbf4d4c72471c2cb86fc15e6253848ab59
MehwishHanif <mehwish.hanif90@gmail.com>
2024-09-24 13:36:34 -0700

    update

--- problems/07-most-vowels.js
+++ problems/07-most-vowels.js
@@ -5,10 +5,42 @@ Vowels are the letters "a", "e", "i", "o", "u".
 */
 
 // Your code here 
+function mostVowels( sentence ){
+    let wordArr=sentence.split(' ');
+    let vowels='aeiou';
+    let vowelCountArr=[]
 
-// console.log(mostVowels("what a wonderful life")); // "wonderful"
-// console.log(mostVowels("the quick brown fox jumps")); // "quick"
-// console.log(mostVowels("")); // ""
+    for(let i = 0; i< wordArr.length; i++){
+        let charArr=wordArr[i].split('');
+        let vowelCount=0;
+        for(let j=0; j < charArr.length; j++){
+            if(vowels.includes(charArr[j]))
+                vowelCount++;
+        }
+        vowelCountArr.push(vowelCount);
+    }
+
+    if (vowelCountArr.length > 0) {
+        let max = vowelCountArr[0];
+        let maxIndex=0;
+        for (let k = 1; k < vowelCountArr.length; k++) {
+            if(max < vowelCountArr[k]){
+                max= vowelCountArr[k];
+                maxIndex=k;
+            }
+        }
+        return wordArr[maxIndex];
+    }
+    else{
+        return '';
+    }
+
+    
+}
+
+console.log(mostVowels("what a wonderful life")); // "wonderful"
+console.log(mostVowels("the quick brown fox jumps")); // "quick"
+console.log(mostVowels("")); // ""
 
 /******************** DO NOT MODIFY ANY CODE BELOW THIS LINE *****************/
 module.exports = mostVowels;
